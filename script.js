eeac7b932b03b97a4df0d0406ee3d539ec422738
Anthony <anthonyblur13@gmail.com>
2026-09-06 17:11:04 -0400

    added exercises and test h1s
    git commit -m

---
+++ script.js
@@ -0,0 +1,33 @@
+const exercises = [
+    { name: "Incline Chest Press", day: "Push", muscleGroup: "Chest", sets: "3", reps: "5-9" },
+    { name: "Shoudler Press", day: "Push", muscleGroup: "Chest", sets: "3", reps: "5-9" },
+    { name: "Pec Dec", day: "Push", muscleGroup: "Chest", sets: "3", reps: "5-9" },
+    { name: "Lateral Raise", day: "Push", muscleGroup: "Chest", sets: "3", reps: "5-9" },
+    { name: "Straight Bar Tricep Pushdown", day: "Push", muscleGroup: "Chest", sets: "3", reps: "5-9" },
+
+
+    { name: "Lateral Puldown", day: "Pull", muscleGroup: "Lats", sets: "3", reps: "5-9" },
+    { name: "Bicep Curl machine", day: "Pull", muscleGroup: "Biceps", sets: "3", reps: "5-9" },
+    { name: "Upper Back Row", day: "Pull", muscleGroup: "Back", sets: "3", reps: "5-9" },
+    { name: "Rope Hammer Curl", day: "Pull", muscleGroup: "Forearms", sets: "3", reps: "5-9" },
+
+
+    { name: "Leg Extensions", day: "Leg Day", muscleGroup: "Quads", sets: "3", reps: "5-9" },
+    { name: "Calf Raises", day: "Leg Day", muscleGroup: "Calves", sets: "3", reps: "5-9" },
+    { name: "Hamstring Cruls", day: "Leg Day", muscleGroup: "Hamstrings", sets: "3", reps: "5-9" },
+    { name: "Leg Press", day: "Leg Day", muscleGroup: "Quads", sets: "3", reps: "5-9" },
+    { name: "Hip Abductors", day: "Leg Day", muscleGroup: "Hips", sets: "3", reps: "5-9" },
+    { name: "Hip Adductors", day: "Leg Day", muscleGroup: "Hips", sets: "3", reps: "5-9" },
+    { name: "Ab Crunch Machine", day: "Leg Day", muscleGroup: "Abs", sets: "3", reps: "5-9" },
+
+
+    { name: "Eliptical", day: "Cardio Day", muscleGroup: "Cardio", time: "1 hour" },
+
+
+]
+
+
+for (exercise of exercises) {
+    console.log(exercise);
+}
+
